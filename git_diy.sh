c6289d8489d24052da19964c7945999db81a10ff
spark <coolloves@gmail.com>
2021-05-06 13:34:03 +0800

    多个文件夹拉取

--- git_diy.sh
+++ git_diy.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 #author:spark
 #需要docker环境,下载本文件到容器内任意位置,以下示例是放到了/jd/scripts
-#举个栗子,我们要拉取大佬i-chenzhe的脚本仓库,则在计划任务内添加以下任务(半小时拉取一次,时间自定):
-#github:https://github.com/monk-coder/dust/tree/dust/i-chenzhe,
-#仓库作者为monk-coder
-#仓库repo为dust
-#仓库文件夹为i-chenzhe
-#添加以下命令到计划任务↓
-#########-> */30 * * * *  bash /jd/scripts/git_diy.sh monk-coder dust i-chenzhe     <-########
+#举个栗子,我们要拉取大佬i-chenzhe的qx脚本仓库,则在计划任务内添加以下任务(半小时拉取一次,时间自定):
+###-> */30 * * * *  /bin/bash /jd/scripts/git_diy.sh  i-chenzhe  qx     <-###
+#只拉取仓库下指定的文件夹,多个用逗号隔开,dira,dirb
+###-> */30 * * * *  /bin/bash /jd/scripts/git_diy.sh  i-chenzhe  qx "dira,dirb"     <-###
 #或者添加到到diy.sh最后一行即可,跟随pull的频率,不过pull的频率远远跟不上柘大佬的节奏!
 #脚本每次运行会检测脚本内的定时任务是否更新,如果有自定义脚本执行时间,不想随脚本更新,请在脚本计划任务后>添加注释 #nochange
 #如果仓库内有不想执行的脚本,注释即可!
@@ -20,14 +17,17 @@
 declare -A BlackListDict
 author=$1
 repo=$2
-dir=$3
+dirs=$3
 
 #私仓更新,秘钥放在/jd/diyscripts/sshkey/目录下,并已作者名字命名
-[ -f /jd/diyscripts/sshkey/$author ] && chmod 600 /jd/diyscripts/sshkey/$author &&  eval `ssh-agent -s` && ssh-add  /jd/diyscripts/sshkey/$author
-#[ -f /jd/diyscripts/sshkey/$author ] &&  export GIT_SSH_COMMAND="ssh -i /jd/diyscripts/sshkey/$author"
+#[ -f /jd/diyscripts/sshkey/$author ] && chmod 600 /jd/diyscripts/sshkey/$author &&  export GIT_SSH_COMMAND="ssh -i /jd/diyscripts/sshkey/$author"
+[ -f /jd/diyscripts/sshkey/$author ] && chmod 600 /jd/diyscripts/sshkey/$author && eval `ssh-agent -s` && ssh-add /jd/diyscripts/sshkey/$author
+[ `grep -c github.com ~/.ssh/known_hosts` -eq 0 ] && ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+
 
 #指定仓库屏蔽关键词,不添加计划任务,多个按照格式二
-BlackListDict['i-chenzhe']="_get|backup"
+BlackListDict['monk-coder']="_get|backup"
 BlackListDict['sparkssssssss']="smzdm|tg|xxxxxxxx"
 
 blackword=${BlackListDict["${author}"]}
@@ -44,9 +44,8 @@ mkdir -p ${diyscriptsdir}
 
 if [ ! -d "$diyscriptsdir/${author}_${repo}" ]; then
   echo -e "${author}本地仓库不存在,从gayhub拉取ing..."
-  cd ${diyscriptsdir} &&  git clone git@github.com:${author}/${repo}.git ${author}_${repo}
+  cd ${diyscriptsdir} &&  git clone https://github.com/${author}/${repo}.git ${author}_${repo}
   gitpullstatus=$?
-  sleep 1000
   [ $gitpullstatus -eq 0 ] && echo -e "${author}本地仓库拉取完毕"
   [ $gitpullstatus -ne 0 ] && echo -e "${author}本地仓库拉取失败,请检查!" && exit 0
 else
@@ -66,14 +65,14 @@ rand(){
 }
 
 function addnewcron {
-  addname=""
+  local addname=""
   if [ -n $dir ];then
     cd ${diyscriptsdir}/${author}_${repo}/$dir
-    author=${author}_${dir}
+    local author=${author}_${dir}
   else
     cd ${diyscriptsdir}/${author}_${repo}
   fi
-  [ $(grep -c "#${author}" /jd/config/crontab.list) -eq 0 ] && sed -i "/hangup/a#${author}" /jd/config/crontab.list
+  [ $(grep -c "#${author}" /jd/config/crontab.list) -eq 0 ] && sed -i "/\#diy/i#${author}" /jd/config/crontab.list
   
   for jspath in `ls *.js|egrep -v $blackword`; 
   #for jspath in `find ./ -name  "*.js"|egrep -v $blackword`; 
@@ -85,8 +84,9 @@ function addnewcron {
       script_date=`cat  $js|grep ^[0-9]|awk '{print $1,$2,$3,$4,$5}'|egrep -v "[a-zA-Z]|:|\."|sort |uniq|head -n 1`
       [ -z "${script_date}" ] && script_date=`cat  $jspath|grep -Eo "([0-9]+|\*|[0-9]+[,-].*) ([0-9]+|\*|[0-9]+[,-].*) ([0-9]+|\*|[0-9]+[,-].*) ([0-9]+|\*|[0-9]+[,-].*) ([0-9]+|\*|[0-9][,-].*)"|sort |uniq|head -n 1`
       [ -z "${script_date}" ] && cron_min=$(rand 1 59) && cron_hour=$(rand 7 9) && script_date="${cron_min} ${cron_hour} * * *"
-      [ $(grep -c -w "$croname" /jd/config/crontab.list) -eq 0 ] && sed -i "/#${author}/a${script_date} bash jd $croname"  /jd/config/crontab.list && addname="${addname}\n${croname}" && echo -e "添加了新的脚本${croname}." && newflag=1;
+      [ $(grep -c -w "$croname" /jd/config/crontab.list) -eq 0 ] && sed -i "/#${author}/a${script_date} bash jd $croname"  /jd/config/crontab.list && addname="${addname}\n${croname}" && echo -e "添加了新的脚本${croname}." && newflag=1 
       [ $newflag -eq 1 ] && bash jd ${croname} now >/dev/null &
+
 
       if [ $(egrep -v "^#|nochange" /jd/config/crontab.list|grep -c -w "$croname" ) -eq 1 ];then
           old_script_date=$(grep -w "$croname" /jd/config/crontab.list|awk '{print $1,$2,$3,$4,$5}')
@@ -101,15 +101,16 @@ function addnewcron {
 
       fi
   done
+  echo $addname
   [ "$addname" != "" ] && [ -f "/jd/scripts/sendinfo.sh" ] && /bin/bash  /jd/scripts/sendinfo.sh "${author}新增自定义脚本" "${addname}"
 
 }
 
 function delcron {
-  delname=""
+  local delname=""
   if [ -n $dir ];then
-    jspath=${diyscriptsdir}/${author}_${repo}/$dir
-    author=${author}_${dir}
+    local jspath=${diyscriptsdir}/${author}_${repo}/$dir
+    local author=${author}_${dir}
   else
     jspath=${diyscriptsdir}/${author}_${repo}
   fi
@@ -126,11 +127,14 @@ function delcron {
 
 if [[ ${gitpullstatus} -eq 0 ]]
 then
-  addnewcron
-  delcron
+  for dir in `echo "$dirs" | sed 's/,/\n/g'`
+  do
+    addnewcron
+    delcron
+  done
 else
   echo -e "$author 仓库更新失败了."
-  [ -f "/jd/scripts/sendinfo.sh" ] && /bin/bash  /jd/scripts/sendinfo.sh "自定义仓库更新失败" "$author"
+  [ -f "/jd/scripts/sendinfo.sh" ] && [ $(date "+%H") -ge 9 ] && [ $(date "+%H") -lt 12 ] && /bin/bash  /jd/scripts/sendinfo.sh "自定义仓库更新失败" "$author"
 fi
 
 exit 0
