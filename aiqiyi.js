f4b1b47e1b0b68b43643763f7451f731380c8ae3
spark <coolloves@gmail.com>
2021-09-16 13:25:19 +0800

    test

---
+++ aiqiyi.js
@@ -0,0 +1,338 @@
+/*
+爱奇艺会员签到脚本
+
+更新时间: 2020.9.6
+脚本兼容: QuantumultX, Surge4, Loon, JsBox, Node.js
+电报频道: @NobyDa
+问题反馈: @NobyDa_bot
+
+获取Cookie说明：
+打开爱奇艺App后(AppStore中国区)，点击"我的", 如通知成功获取cookie, 则可以使用此签到脚本.
+获取Cookie后, 请将Cookie脚本禁用并移除主机名，以免产生不必要的MITM.
+脚本将在每天上午9:00执行, 您可以修改执行时间。
+
+如果使用Node.js, 需自行安装'request'模块. 例: npm install request -g
+
+JsBox, Node.js用户抓取Cookie说明：
+开启抓包, 打开爱奇艺App后(AppStore中国区)，点击"我的" 返回抓包App 搜索请求头关键字 psp_cki= 或 P00001= 或 authcookie=
+提取字母数字混合字段, 到&结束, 填入以下单引号内即可.
+
+# rewrite
+https?://community.iqiyi.com/openApi/task/execute\?.*   aiqiyi.js
+
+# task
+8 8 * * * aiqiyi.js
+
+[Mitm] 
+hostname= community.iqiyi.com
+*/
+const $ = new Env("aiqiyi");
+const notify = $.isNode() ? require('./sendNotify') : '';
+message = ""
+let iqiyi_cookie= $.isNode() ? (process.env.iqiyi_cookie ? process.env.iqiyi_cookie : "") : ($.getdata('iqiyi_cookie') ? $.getdata('iqiyi_cookie') : "")
+let iqiyi_cookieArr = []
+let iqiyi_cookies = ""
+
+if (typeof $request !== "undefined") {
+   getiqiyi_cookie()
+    $.done()
+}
+
+async function getiqiyi_cookie() {
+    if ($request.url.match(/\/openApi\/task\/execute/)) {
+        authcookie = $request.url.match(/authCookie=([a-zA-Z0-9]+)&/)[1]
+        //authcookie = $request.url.match(/authCookie=([a-zA-Z0-9]+)/)[1]
+
+    }
+
+        if (iqiyi_cookie) {
+            if (iqiyi_cookie.indexOf(authcookie) > -1) {
+                $.log("此cookie已存在，本次跳过")
+            } else if (iqiyi_cookie.indexOf(userid) === -1) {
+                iqiyi_cookies = iqiyi_cookie + "@" + authcookie;
+                console.log(authcookie)
+                $.setdata(iqiyi_cookies, 'iqiyi_cookie');
+                $.log(`${$.name}获取cookie: 成功, iqiyi_cookies: ${bodyVal}`);
+                bodys = iqiyi_cookies.split("@")
+            }
+         } else {
+            $.setdata(authcookie, 'iqiyi_cookie');
+            iqiyi_cookie = authcookie
+            console.log(authcookie)
+            $.log(`${$.name}获取cookie: 成功, iqiyi_cookies: ${authcookie}`);
+            $.msg($.name, `获取第一个cookie: 成功`,`${authcookie}`);
+       }
+}
+
+var cookie = iqiyi_cookie
+
+if (!cookie) {$.msg("iQiyi_error", "","签到终止, 未获取Cookie");}
+
+
+var LogDetails = false; // 响应日志
+var out = 0; // 超时 (毫秒) 如填写, 则不少于3000
+
+var $nobyda = nobyda();
+
+(async () => {
+  out = $nobyda.read("iQIYI_TimeOut") || out
+  cookie = cookie || $nobyda.read("CookieQY")
+  LogDetails = $nobyda.read("iQIYI_LogDetails") === "true" ? true : LogDetails
+  if ($nobyda.isRequest) {
+    //GetCookie()
+    getiqiyi_cookie()
+  } else if (cookie) {
+    await login();
+    await Checkin();
+    await Lottery(500);
+    await $nobyda.time();
+  } else {
+    await notify.sendNotify("爱奇艺会员", "", "签到终止, 未获取Cookie");
+    $.msg("iQiyi_error", "签到终止, 未获取Cookie","");
+  }
+})().finally(() => {
+  $nobyda.done();
+})
+
+function login() {
+  return new Promise(resolve => {
+    var URL = {
+      url: 'https://cards.iqiyi.com/views_category/3.0/vip_home?secure_p=iPhone&scrn_scale=0&dev_os=0&ouid=0&layout_v=6&psp_cki=' + cookie + '&page_st=suggest&app_k=8e48946f144759d86a50075555fd5862&dev_ua=iPhone8%2C2&net_sts=1&cupid_uid=0&xas=1&init_type=6&app_v=11.4.5&idfa=0&app_t=0&platform_id=0&layout_name=0&req_sn=0&api_v=0&psp_status=0&psp_uid=451953037415627&qyid=0&secure_v=0&req_times=0',
+      headers: {
+        sign: '7fd8aadd90f4cfc99a858a4b087bcc3a',
+        t: '479112291'
+      }
+    }
+    $nobyda.get(URL, function(error, response, data) {
+      const Details = LogDetails ? data ? `response:\n${data}` : '' : ''
+      if (!error && data.match(/\"text\":\"\d.+?\u5230\u671f\"/)) {
+        $nobyda.expire = data.match(/\"text\":\"(\d.+?\u5230\u671f)\"/)[1]
+        console.log(`爱奇艺-查询成功: ${$nobyda.expire} ${Details}`)
+      } else {
+        console.log(`爱奇艺-查询失败${error || ': 无到期数据 ⚠️'} ${Details}`)
+      }
+      resolve()
+    })
+    if (out) setTimeout(resolve, out)
+  })
+}
+
+function Checkin() {
+  return new Promise(resolve => {
+    var URL = {
+      url: 'https://tc.vip.iqiyi.com/taskCenter/task/queryUserTask?autoSign=yes&P00001=' + cookie
+    }
+    $nobyda.get(URL, function(error, response, data) {
+      if (error) {
+        $nobyda.data = "签到失败: 接口请求出错 ‼️"
+        console.log(`爱奇艺-${$nobyda.data} ${error}`)
+      } else {
+        const obj = JSON.parse(data)
+        const Details = LogDetails ? `response:\n${data}` : ''
+        if (obj.msg == "成功") {
+          if (obj.data.signInfo.code == "A00000") {
+            var AwardName = obj.data.signInfo.data.rewards[0].name;
+            var quantity = obj.data.signInfo.data.rewards[0].value;
+            var continued = obj.data.signInfo.data.continueSignDaysSum;
+            $nobyda.data = "签到成功: " + AwardName + quantity + ", 已连签" + continued + "天 🎉"
+            console.log(`爱奇艺-${$nobyda.data} ${Details}`)
+          } else {
+            $nobyda.data = "签到失败: " + obj.data.signInfo.msg + " ⚠️"
+            console.log(`爱奇艺-${$nobyda.data} ${Details}`)
+          }
+        } else {
+          $nobyda.data = "签到失败: Cookie无效 ⚠️"
+	  notify.sendNotify("iQiyi_error",`${Details}`)
+          $.msg("iQiyi_error", "",`${Details}`);
+          console.log(`爱奇艺-${$nobyda.data} ${Details}`)
+        }
+      }
+      resolve()
+    })
+    if (out) setTimeout(resolve, out)
+  })
+}
+
+function Lottery(s) {
+  return new Promise(resolve => {
+    $nobyda.times++
+      const URL = {
+        url: 'https://iface2.iqiyi.com/aggregate/3.0/lottery_activity?app_k=0&app_v=0&platform_id=0&dev_os=0&dev_ua=0&net_sts=0&qyid=0&psp_uid=0&psp_cki=' + cookie + '&psp_status=0&secure_p=0&secure_v=0&req_sn=0'
+      }
+    setTimeout(() => {
+      $nobyda.get(URL, async function(error, response, data) {
+        if (error) {
+          $nobyda.data += "\n抽奖失败: 接口请求出错 ‼️"
+          console.log(`爱奇艺-抽奖失败: 接口请求出错 ‼️ ${error} (${$nobyda.times})`)
+	  notify.sendNotify("iQiyi_error",`${error}`)
+          $.msg("iQiyi_error", "", `${error}`);
+          //notify.sendNotify("爱奇艺", "", $nobyda.data)
+        } else {
+          const obj = JSON.parse(data);
+          const Details = LogDetails ? `response:\n${data}` : ''
+          $nobyda.last = data.match(/(机会|已经)用完|登录信息失效/) ? true : false
+          $nobyda.res = data.match(/登录信息失效/) ? true : false
+	  //console.log(obj)	
+	  //console.log($nobyda.last)
+          if (obj.awardName && obj.code == 0) {
+            $nobyda.data += !$nobyda.last ? `\n抽奖成功: ${obj.awardName.replace(/《.+》/, "未中奖")} 🎉` : `\n抽奖失败: 今日已抽奖 ⚠️`
+            console.log(`爱奇艺-抽奖明细: ${obj.awardName.replace(/《.+》/, "未中奖")} 🎉 (${$nobyda.times}) ${Details}`)
+          } else if (data.match(/\"errorReason\"/)) {
+            msg = data.match(/msg=.+?\)/) ? data.match(/msg=(.+?)\)/)[1].replace(/用户(未登录|不存在)/, "Cookie无效") : ""
+            $nobyda.data += `\n抽奖失败: ${msg || `未知错误`} ⚠️`
+            console.log(`爱奇艺-抽奖失败: ${msg || `未知错误`} ⚠️ (${$nobyda.times}) ${msg ? Details : `response:\n${data}`}`)
+            //notify.sendNotify("iQiyi_error"," Cookie无效")
+          } else {
+            $nobyda.data += "\n抽奖错误: 已输出日志 ⚠️"
+            console.log(`爱奇艺-抽奖失败: \n${data} (${$nobyda.times})`)
+            //notify.sendNotify("iQiyi_error"," Cookie无效")
+          }
+        }
+	if ($nobyda.res) {
+	    notify.sendNotify("iQiyi_error"," Cookie无效")	
+            $.msg("iQiyi_error", "","Cookie无效");
+	}
+        if (!$nobyda.last && $nobyda.times < 3) {
+          await Lottery(s)
+        } else {
+          const expires = $nobyda.expire ? $nobyda.expire.replace(/\u5230\u671f/, "") : "获取失败 ⚠️"
+          if (!$nobyda.isNode) notify.sendNotify("爱奇艺", "到期时间: " + expires, $nobyda.data)
+        }
+        resolve()
+      })
+    }, s)
+    if (out) setTimeout(resolve, out + s)
+  })
+}
+
+function GetCookie() {
+  var CKA = $request.url.match(/(psp_cki=|P00001=|authcookie=)([A-Za-z0-9]+)/)
+  var CKB = JSON.stringify($request.headers).match(/(psp_cki=|P00001=|authcookie=)([A-Za-z0-9]+)/)
+  var iQIYI = CKA || CKB || null
+  var RA = $nobyda.read("CookieQY")
+  if (iQIYI) {
+    if (RA != iQIYI[2]) {
+      var OldTime = $nobyda.read("CookieQYTime")
+      if (!$nobyda.write(iQIYI[2], "CookieQY")) {
+        notify.sendNotify(`${RA?`更新`:`首次写入`}爱奇艺签到Cookie失败‼️`, "", "")
+      } else {
+        if (!OldTime || OldTime && (Date.now() - OldTime) / 1000 >= 21600) {
+          $nobyda.write(JSON.stringify(Date.now()), "CookieQYTime")
+          notify.sendNotify(`${RA?`更新`:`首次写入`}爱奇艺签到Cookie成功 🎉`, "", "")
+        } else {
+          console.log(`\n更新爱奇艺Cookie成功! 🎉\n检测到频繁通知, 已转为输出日志`)
+        }
+      }
+    } else {
+      console.log("\n爱奇艺-与本机储存Cookie相同, 跳过写入 ⚠️")
+    }
+  } else {
+    console.log("\n爱奇艺-请求不含Cookie, 跳过写入 ‼️")
+  }
+}
+
+function nobyda() {
+  const times = 0
+  const start = Date.now()
+  const isRequest = typeof $request != "undefined"
+  const isSurge = typeof $httpClient != "undefined"
+  const isQuanX = typeof $task != "undefined"
+  const isLoon = typeof $loon != "undefined"
+  const isJSBox = typeof $app != "undefined" && typeof $http != "undefined"
+  const isNode = typeof require == "function" && !isJSBox;
+  const node = (() => {
+    if (isNode) {
+      const request = require('request');
+      return ({
+        request
+      })
+    } else {
+      return (null)
+    }
+  })()
+  const notify = (title, subtitle, message) => {
+    if (isQuanX) $notify.sendNotify(title, subtitle, message)
+    if (isSurge) $notification.post(title, subtitle, message)
+    if (isNode) log('\n' + title + '\n' + subtitle + '\n' + message)
+    if (isJSBox) $push.schedule({
+      title: title,
+      body: subtitle ? subtitle + "\n" + message : message
+    })
+  }
+  const write = (value, key) => {
+    if (isQuanX) return $prefs.setValueForKey(value, key)
+    if (isSurge) return $persistentStore.write(value, key)
+  }
+  const read = (key) => {
+    if (isQuanX) return $prefs.valueForKey(key)
+    if (isSurge) return $persistentStore.read(key)
+  }
+  const adapterStatus = (response) => {
+    if (response) {
+      if (response.status) {
+        response["statusCode"] = response.status
+      } else if (response.statusCode) {
+        response["status"] = response.statusCode
+      }
+    }
+    return response
+  }
+  const get = (options, callback) => {
+    if (isQuanX) {
+      if (typeof options == "string") options = {
+        url: options
+      }
+      options["method"] = "GET"
+      $task.fetch(options).then(response => {
+        callback(null, adapterStatus(response), response.body)
+      }, reason => callback(reason.error, null, null))
+    }
+    if (isSurge) $httpClient.get(options, (error, response, body) => {
+      callback(error, adapterStatus(response), body)
+    })
+    if (isNode) {
+      node.request(options, (error, response, body) => {
+        callback(error, adapterStatus(response), body)
+      })
+    }
+    if (isJSBox) {
+      if (typeof options == "string") options = {
+        url: options
+      }
+      options["header"] = options["headers"]
+      options["handler"] = function(resp) {
+        let error = resp.error;
+        if (error) error = JSON.stringify(resp.error)
+        let body = resp.data;
+        if (typeof body == "object") body = JSON.stringify(resp.data);
+        callback(error, adapterStatus(resp.response), body)
+      };
+      $http.get(options);
+    }
+  }
+
+  const log = (message) => console.log(message)
+  const time = () => {
+    const end = ((Date.now() - start) / 1000).toFixed(2)
+    return console.log('\n签到用时: ' + end + ' 秒')
+  }
+  const done = (value = {}) => {
+    if (isQuanX) return $done(value)
+    if (isSurge) isRequest ? $done(value) : $done()
+  }
+  return {
+    isRequest,
+    isNode,
+    notify,
+    write,
+    read,
+    get,
+    log,
+    time,
+    times,
+    done
+  }
+};
+
+
+function Env(t,e){"undefined"!=typeof process&&JSON.stringify(process.env).indexOf("GITHUB")>-1&&process.exit(0);class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon()?(this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)})):this.isQuanX()?(this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t))):this.isNode()&&(this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)}))}post(t,e=(()=>{})){if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.post(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method="POST",this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){this.initGotEnv(t);const{url:s,...i}=t;this.got.post(s,i).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)})}}time(t){let e={"M+":(new Date).getMonth()+1,"d+":(new Date).getDate(),"H+":(new Date).getHours(),"m+":(new Date).getMinutes(),"s+":(new Date).getSeconds(),"q+":Math.floor(((new Date).getMonth()+3)/3),S:(new Date).getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,((new Date).getFullYear()+"").substr(4-RegExp.$1.length)));for(let s in e)new RegExp("("+s+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?e[s]:("00"+e[s]).substr((""+e[s]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl;return{"open-url":e,"media-url":s}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r)));let h=["","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];h.push(e),s&&h.push(s),i&&h.push(i),console.log(h.join("\n")),this.logs=this.logs.concat(h)}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
